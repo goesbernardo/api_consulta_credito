9478b011c675953ea86ef615e0e1cf944dc04d93
Bernardo Goes <bernardo.goes@infobase.com.br>
2025-03-15 16:43:44 -0300

    commit estrutura projeto

---
+++ src/main/java/com/goesbernardo/api_cartao/ApiCartaoApplication.java
@@ -0,0 +1,13 @@
+package com.goesbernardo.api_cartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCartaoApplication.class, args);
+	}
+
+}
